b8d548a86e119f888eb4f7ec09ab0c82ce23418d
Andreas MOUZONG <andreasmouzong@gmail.com>
2024-10-03 06:50:52 +0200

    Added @CsvSource in place of @MethodSource

--- test/tech/codegrilldeveloperblog/CalculatorTest.java
+++ test/tech/codegrilldeveloperblog/CalculatorTest.java
@@ -3,6 +3,7 @@ package tech.codegrilldeveloperblog;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -77,10 +78,24 @@ class CalculatorTest {
 
     @DisplayName("Test Integer subtraction [minuend, subtrahend, expectedResult]")
     @ParameterizedTest
-    @MethodSource()
+//    @MethodSource()
+//    @CsvSource({
+//            "33, 1, 32",
+//            "24, 1, 23",
+//            "58, 52, 6",
+//            "12, 4, 8",
+//            "33, 23, 10",
+//            "7, 3, 4",
+//    })
+    @CsvSource({
+            "apple, orange",
+            "apple, strawberry",
+            "apple, ''",
+            "apple, ",
+    })
     void integerSubtraction(int minuend, int subtrahend, int expectedResult) {
 
-        System.out.println("Running Test "+minuend+" - "+subtrahend+" = "+expectedResult);
+        System.out.println("Running Test " + minuend + " - " + subtrahend + " = " + expectedResult);
 
         // Act  // When
         int result = calculator.integerSubtraction(minuend, subtrahend);
@@ -90,11 +105,11 @@ class CalculatorTest {
                 () -> minuend + " - " + subtrahend + " did not produce " + result);
     }
 
-    private static Stream<Arguments> integerSubtraction(){
-        return Stream.of(
-                Arguments.of(5,3,2),
-                Arguments.of(54,1,53),
-                Arguments.of(24,6,18)
-        );
-    }
+//    private static Stream<Arguments> integerSubtraction() {
+//        return Stream.of(
+//                Arguments.of(5, 3, 2),
+//                Arguments.of(54, 1, 53),
+//                Arguments.of(24, 6, 18)
+//        );
+//    }
 }
